36c38ce080dcaf4c906fad07137ae7805dd5f3b8
FELIPE FERREIRA DA SILVA <2993620@fmu.interno>
2023-06-06 20:56:12 -0300

    criado conexao com pagina de exemplo via PHP

---
+++ html/login/login.php
@@ -0,0 +1,24 @@
+<?php
+function debug_to_console($data)
+{
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
+
+
+function connectPoke()
+{
+
+    $url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $resp = curl_exec($curl);
+    curl_close($curl);
+    echo $resp;
+}
+?>
